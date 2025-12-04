b362063205477a050a31cbd6986f99895ff1d31b
MicroTools-Hub <malkapurwalahuzeifa123@gmail.com>
2025-12-04 11:22:50 +0530

    Adsense

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import "../styles/global.css";
 import Layout from "../components/Layout";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        {/* Google AdSense Verification */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6119998481340838"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
+
